f4ab2f0aa67e524f2d8f4159fc9c432f04f4fdef
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-08-09 19:25:52 +0200

    123

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener("click", handleClick);
+
+function createMarcup() {
+    let marcup = "";
+    for (let i = 0; i < galleryItems.length; i++) {
+        marcup += `<li class="gallery__item">
+        <a class="gallery__link" href="${galleryItems[i].original}">
+          <img
+            class="gallery__image"
+            src="${galleryItems[i].preview}"
+            data-source="${galleryItems[i].original}"
+            alt="${galleryItems[i].description}"
+          />
+        </a>
+      </li>`
+    }
+    gallery.innerHTML = marcup;
+}
+
+createMarcup();
+
+function handleClick(event) {
+    event.preventDefault()
+    for (let i = 0; i < galleryItems.length; i++) {
+        if(event.target.getAttribute("src") === galleryItems[i].preview) {
+            const instance = basicLightbox.create(`
+            <img src="${galleryItems[i].original}" width="800" height="600">
+        `);
+        instance.show()
+        document.body.addEventListener("keydown", (e) => {
+            if(e.code === "Escape") {
+                instance.close();
+                        }
+        });
+        }
+    }
+}
