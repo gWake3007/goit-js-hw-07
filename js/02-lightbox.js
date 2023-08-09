a026c085230f964ad6d9a51cb463728e3f2063b6
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-08-09 20:37:50 +0200

    123

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,3 +3,27 @@ import { galleryItems } from './gallery-items.js';
 
 // console.log(galleryItems);
 
+const gallery = document.querySelector('.gallery');
+
+const galleryMarcup = createGalleryMarcup(galleryItems);
+
+function createGalleryMarcup(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) =>{
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+     </li>`;
+    })
+    .join("");
+}
+
+gallery.insertAdjacentHTML("beforeend", galleryMarcup);
+
+let lightBox = new SimpleLightbox ('.gallery a', {
+    captions: 'true',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
